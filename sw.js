510b9bda81880ac36a42568c862a22834616a2b8
pedronoriega-eng <pedro.noriega@gmail.com>
2026-04-30 10:09:38 -0500

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'chemsafe-v1';
+const urlsToCache = [
+  './demo.html',
+  './acido sulfurico.png',
+  './Hipoclorito de Sodio.png',
+  './FDS_Acetona.pdf',
+  './FDS_acido sulfurico.pdf',
+  './FDS_Hipoclorito de Sodio.pdf'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
